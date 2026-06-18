f00bee56cb9cb50a475c915d0aeaff51efb9b62d
x-c-x-c <xcx489743@gmail.com>
2026-06-18 20:55:57 -0300

    adiciona permissoes e funcionalidade de foto de perfil (camera e galeria)

--- App.tsx
+++ App.tsx
@@ -9,45 +9,52 @@ import { useEffect } from "react";
 import { signInRequest } from "./src/services/auth";
 import Toast from "react-native-toast-message";
 import { AuthProvider, useAuth } from "./src/contexts/AuthContext";
+import { Perfil } from './src/routers/pagesTestes/Perfil';
+
 
 
 export default function App() {
+  return <Perfil />;
+}
+//export default function App() {
 
   //Teste contexto+STORAGE
-  function TesteDoLogin() {
-    const { signIn, user, loading } = useAuth()
+  //function TesteDoLogin() {
+  //  const { signIn, user, loading } = useAuth()
+//
+  //  useEffect(() => {
+  //    console.log('Iniciando Login')
+  //    signIn({ email: 'jojo@teste.com', senha: '123123' })
+  //      .catch(e => console.log('Deu Ruim: ', e.message))
+  //  }, [])
+  //  useEffect(() => {
+  //    console.log('o usuario logado é : ', user)
+  //  })
+//
+  //  return null
+  //}
 
-    useEffect(() => {
-      console.log('Iniciando Login')
-      signIn({ email: 'jojo@teste.com', senha: '123123' })
-        .catch(e => console.log('Deu Ruim: ', e.message))
-    }, [])
-    useEffect(() => {
-      console.log('o usuario logado é : ', user)
-    })
-
-    return null
-  }
 
 
-  return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.tsx to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
 
-    // <ThemeProvider theme={theme}>
-    <AuthProvider>
-      <NavigationContainer>
-        <Routers />
-        <TesteDoLogin />
-        <Toast />
-      </NavigationContainer>
-    </AuthProvider>
-
-    // </ThemeProvider>
-  );
-}
+//eturn (
+// // <View style={styles.container}>
+// //   <Text>Open up App.tsx to start working on your app!</Text>
+// //   <StatusBar style="auto" />
+// // </View>
+//
+// // <ThemeProvider theme={theme}>
+// <AuthProvider>
+//   <NavigationContainer>
+//     <Routers />
+//     <TesteDoLogin />
+//     <Toast />
+//   </NavigationContainer>
+// </AuthProvider>
+//
+// // </ThemeProvider>
+//;
+//
 
 // const styles = StyleSheet.create({
 //   container: {
---
+++ src/routers/pagesTestes/Perfil/style.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 16,
+    },
+    foto: {
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+    },
+});
