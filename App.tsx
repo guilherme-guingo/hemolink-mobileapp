3ffc456d0bf31bc9a96611b51da8dde5b444b876
x-c-x-c <xcx489743@gmail.com>
2026-06-18 21:04:16 -0300

    remove codigo de teste do perfil no app.tsx

--- App.tsx
+++ App.tsx
@@ -11,50 +11,49 @@ import Toast from "react-native-toast-message";
 import { AuthProvider, useAuth } from "./src/contexts/AuthContext";
 import { Perfil } from './src/routers/pagesTestes/Perfil';
 
+//teste perfil abrir camera e escolher da galeria
+//export default function App() {
+//  return <Perfil />;
+//}
 
 
 export default function App() {
-  return <Perfil />;
-}
-//export default function App() {
 
   //Teste contexto+STORAGE
-  //function TesteDoLogin() {
-  //  const { signIn, user, loading } = useAuth()
-//
-  //  useEffect(() => {
-  //    console.log('Iniciando Login')
-  //    signIn({ email: 'jojo@teste.com', senha: '123123' })
-  //      .catch(e => console.log('Deu Ruim: ', e.message))
-  //  }, [])
-  //  useEffect(() => {
-  //    console.log('o usuario logado é : ', user)
-  //  })
-//
-  //  return null
-  //}
+  function TesteDoLogin() {
+    const { signIn, user, loading } = useAuth()
+
+    useEffect(() => {
+      console.log('Iniciando Login')
+      signIn({ email: 'jojo@teste.com', senha: '123123' })
+        .catch(e => console.log('Deu Ruim: ', e.message))
+    }, [])
+    useEffect(() => {
+      console.log('o usuario logado é : ', user)
+    })
+
+    return null
+  }
 
 
+  return (
+    // <View style={styles.container}>
+    //   <Text>Open up App.tsx to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
 
+    // <ThemeProvider theme={theme}>
+    <AuthProvider>
+      <NavigationContainer>
+        <Routers />
+        <TesteDoLogin />
+        <Toast />
+      </NavigationContainer>
+    </AuthProvider>
 
-//eturn (
-// // <View style={styles.container}>
-// //   <Text>Open up App.tsx to start working on your app!</Text>
-// //   <StatusBar style="auto" />
-// // </View>
-//
-// // <ThemeProvider theme={theme}>
-// <AuthProvider>
-//   <NavigationContainer>
-//     <Routers />
-//     <TesteDoLogin />
-//     <Toast />
-//   </NavigationContainer>
-// </AuthProvider>
-//
-// // </ThemeProvider>
-//;
-//
+    // </ThemeProvider>
+  );
+}
 
 // const styles = StyleSheet.create({
 //   container: {
