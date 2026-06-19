2e9a8237bd2a532845f71c7e0a95048b419bf0f3
LuizFelipe <luizfelipe.vor@gmail.com>
2026-06-18 22:05:02 -0300

    tipagem do catalogo dos hospitais

--- src/services/HospitalService.ts
+++ src/services/HospitalService.ts
@@ -1,12 +1,31 @@
 import { catalogoHospitalApi as api } from "./api/api";
 
 
-
 export interface Hospital {
-    id: string;
-    nome: string,
-    endereco: string,
-    telefone: string
+  id: string;
+  image: string;
+  name: string;
+  cnpj: string;
+  address: string;
+  city: string;
+  state: string;
+  cep: string;
+  phone: string;
+  email: string;
+  website: string;
+  openingHours: string;
+  bloodStock: BloodStock;
+}
+
+export interface BloodStock {
+  "A+": number;
+  "A-": number;
+  "B+": number;
+  "B-": number;
+  "AB+": number;
+  "AB-": number;
+  "O+": number;
+  "O-": number;
 }
 
 export async function listarHospitais(): Promise<Hospital[]> {
